8969e51d4db9df6728280b91367e1b6a9d6d2364
ben <ben.loh.15@ucl.ac.uk>
2018-08-03 16:04:38 +0100

    Add mesh_renderer runner (requires tf_mesh_renderer)

--- test_runner_mesh_render.py
+++ test_runner_mesh_render.py
@@ -5,8 +5,10 @@ import sys
 import os
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 from .batch_smpl import SMPL
+from ..tf_mesh_renderer import mesh_renderer
 
 
 if __name__ == '__main__':
@@ -38,3 +40,25 @@ if __name__ == '__main__':
 
         for f in faces + 1:
             fp.write('f %d %d %d\n' % (f[0], f[1], f[2]))
+
+    in_verts = tf.constant(result, dtype=tf.float32)[tf.newaxis]
+    in_normals = tf.nn.l2_normalize(in_verts, axis=1)
+    faces = tf.constant(faces, dtype=tf.int32)
+
+    eye = tf.constant([[0.0, 0.0, 6.0]], dtype=tf.float32)
+    center = tf.constant([[0.0, 0.0, 0.0]], dtype=tf.float32)
+    world_up = tf.constant([[0.0, 1.0, 0.0]], dtype=tf.float32)
+    image_width, image_height = 640, 480
+    light_positions = tf.constant([[[0.0, 0.0, 6.0]]])
+    light_intensities = tf.zeros([1, 1, 3], dtype=tf.float32)
+    vertex_diffuse_colors = tf.ones_like(in_verts, dtype=tf.float32)
+
+    rendered = mesh_renderer.mesh_renderer(
+        in_verts, faces, in_normals,
+        vertex_diffuse_colors, eye, center, world_up, light_positions,
+        light_intensities, image_width, image_height)
+
+    img = sess.run(rendered)
+    print(img.shape)
+    plt.imshow(img[0])
+    plt.show()
